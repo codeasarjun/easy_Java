e9208137ea03a8347bd1f13bba82d174bef6033d
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-11-08 17:50:51 +0530

    taking input from scanner

---
+++ taking_Input/scannerClass.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class HelloWorld
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int x;
+	    long y;
+	    char c;
+	    double d;
+	    String s;
+	    x=sc.nextInt();
+	    //y=sc.nextLong();
+	    //c=sc.next().charAt(0);// to get char
+	    //s=sc.nextLine();
+	    //d=sc.nextDouble();
+	    sc.close();
+	    
+	    
+	    
+		System.out.println(x);
+	}
+	}

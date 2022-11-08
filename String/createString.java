f23a5291942da67f8e19ad6fff8249948bca31e6
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-11-08 19:27:30 +0530

    creating String

---
+++ String/createString.java
@@ -0,0 +1,22 @@
+/*
+There are two ways to create string in Java:
+
+String literal
+String s = “Have Fun”;
+
+
+Using new keyword
+String s = new String (“have fun”);
+
+*/
+
+class createString {
+    
+    
+    public static void main(String[] args) {
+        String s = "Have Fun";
+        String ss=new String ("have fun");
+        System.out.println(ss);
+    }
+}
+

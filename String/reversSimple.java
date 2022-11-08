b4fbcc40bd7f0476e999ce39a97ca494ebec42fd
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-11-08 22:24:29 +0530

    reverse a String in simple way

---
+++ String/reversSimple.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    
+    
+    public static void main(String[] args) {
+        String s="op";
+        String res="";
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            res+=s.charAt(i);
+        
+        }
+        System.out.println(res);
+        
+
+
+    }
+}

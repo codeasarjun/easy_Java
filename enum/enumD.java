4fe8eba20e74d8aaef4d432ed5822acf7988a19e
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-11-08 11:49:14 +0530

    enum declaration

---
+++ enum/enumD.java
@@ -0,0 +1,18 @@
+
+
+enum Color {
+    RED,
+    GREEN,
+    BLUE;
+}
+
+public class enumD {
+    
+    public static void main(String[] args)
+    {
+        Color c1 = Color.RED;  //calling Red
+        System.out.println(c1);
+    }
+}
+
+
